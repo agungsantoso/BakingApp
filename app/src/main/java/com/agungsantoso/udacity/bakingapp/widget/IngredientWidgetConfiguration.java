819a1b7e62fd41cc16daa1a5f24adce33b78472b
Agung Santoso <agung.santoso.pranoto@gmail.com>
2017-09-14 14:43:08 +0700

    feat: Create Widget

---
+++ app/src/main/java/com/agungsantoso/udacity/bakingapp/widget/IngredientWidgetConfiguration.java
@@ -0,0 +1,20 @@
+package com.agungsantoso.udacity.bakingapp.widget;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.agungsantoso.udacity.bakingapp.R;
+
+public class IngredientWidgetConfiguration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_widget_configuration);
+    }
+
+}
---
+++ app/src/main/java/com/agungsantoso/udacity/bakingapp/widget/IngredientWidgetProvider.java
@@ -0,0 +1,11 @@
+package com.agungsantoso.udacity.bakingapp.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by asanpra on 14/09/2017.
+ */
+
+public class IngredientWidgetProvider extends AppWidgetProvider {
+
+}
